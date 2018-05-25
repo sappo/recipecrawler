28e6e5c54091740baa8c608d6fb9cd894085c90b
sappo <mail@kevinsapper.de>
2018-05-25 13:56:50 +0200

    Problem: No parser for eatsmarter
    Solution: Write a spider that parses eatsmarter.de website

--- recipecrawl/spiders/eatsmarter.py
+++ recipecrawl/spiders/eatsmarter.py
@@ -1,34 +1,110 @@
+import re
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, Identity
 
+class Recipe(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    ingrediants = scrapy.Field()
+    nutritions = scrapy.Field()
 
-class QuotesSpider(scrapy.Spider):
-    name = "eatsmarter"
+class RecipeLoader(ItemLoader):
+    default_output_processor = TakeFirst()
 
-    def start_requests(self):
-        # Spiders can be started with optional attributes via '-a attribute=value'
-        # attribute = getattr(self, 'attribute', None)
-        #   if attribute is not None:
-        #       pass
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    def parse_ingrediants(self, ingrediantNodes):
+        ingrediants = []
+        for ingrediantNode in ingrediantNodes:
+            ingrediantParts = ingrediantNode.xpath("descendant::*[not(contains(@class, 'shoplink'))]/text()").extract()
+            ingrediant = " ".join(part.strip() for part in ingrediantParts)
+            ingrediant = re.sub(' +',' ',ingrediant)
+            ingrediants.append(ingrediant)
+
+        return ingrediants
+
+    ingrediants_in = parse_ingrediants
+    ingrediants_out = Identity()
+
+class EatsmarterSpider(scrapy.Spider):
+    name = 'eatsmarter.de'
+    allowed_domains = ['eatsmarter.de']
+    start_urls = ['https://eatsmarter.de/rezepte/vegetarische-nudelpfanne']
+    le = LinkExtractor(allow=('\/rezepte\/[^\/#?&]+(?=$)'),
+                       allow_domains=allowed_domains)
+    crawled_urls = set()
+
+    def parse_nutrition_type_unit(self, type):
+        unit = ''
+        if type.find('/') > -1:
+            # Drop Unit
+            partition = type.rpartition('/')
+            type = partition[0]
+            unit = partition[2]
+
+        if type == 'Ballaststoffe':
+            type = 'Dietary Fiber'
+        if type == 'BE':
+            type = 'Bread Unit'
+        if type == 'Brennwert':
+            type = 'Calories'
+        if type == 'Cholesterin':
+            type = 'Cholesterol'
+        if type == 'Eiweiß/Protein':
+            type = 'Protein'
+        if type == 'Eisen':
+            type = 'Iron'
+        if type == 'Fett':
+            type = 'Total Fat'
+        if type == 'Folsäure':
+            type = 'Folic Acid'
+        if type == 'Harnsäure':
+            type = 'Uric Acid'
+        if type == 'gesättigte Fettsäuren':
+            type = 'Saturated Fat'
+        if type == 'Jod':
+            type = 'Iodine'
+        if type == 'Kohlenhydrate':
+            type = 'Total Carbohydrate'
+        if type == 'Kallium':
+            type = 'Potassium'
+        if type == 'Kalzium':
+            type = 'Calcium'
+        if type == 'Pantothensäure':
+            type = 'Pantothenic Acid'
+        if type == 'zugesetzter Zucker':
+            type = 'Sugars'
+        if type == 'Zink':
+            type = 'Zinc'
+
+        return (type, unit)
 
     def parse(self, response):
-        # Yield'ed json can be saved with: scrapy crawl <spider> -o output.json
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
-            }
+        if response.css('.recipe-top'):
+            self.logger.info('Hi, this is an item page! %s', response.url)
+            recipe = RecipeLoader(item=Recipe(), response=response)
+            recipe.add_value('url', response.url)
+            recipe.add_xpath('title', "//*[@class='fn title p-name']/text()")
+            recipe.add_value('ingrediants', response.xpath("//*[contains(@class, 'ingredients')]//*[contains(@class, 'ingredient')]"))
+            yield response.follow(response.url + '/foodcheck#recipe-tabs',
+                                  callback=self.parse_nutritions,
+                                  meta={'recipe':recipe})
 
-        # Yield'ed scrapy.Requests follows other links
-        next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
-            # Or
-            # yield response.follow(next_page, callback=self.parse)
+        for link in (link for link in self.le.extract_links(response) if link.url not in self.crawled_urls):
+            self.crawled_urls.add(link.url)
+            yield scrapy.Request(link.url, callback=self.parse)
+
+    def parse_nutritions(self, response):
+        nutritions = {}
+        for nutritionNode in response.xpath("//*[contains(@class,'nutritions-table')]//tr"):
+            tdNodes = nutritionNode.xpath("descendant::td/text()").extract()
+            type_unit = tdNodes[0]
+            value = tdNodes[1]
+            type_unit = self.parse_nutrition_type_unit(type_unit)
+            nutritions[type_unit[0]] = {'Unit': type_unit[1], 'Value:': value}
+
+        recipe = response.meta['recipe']
+        recipe.add_value('nutritions', nutritions)
+
+        yield recipe.load_item()
